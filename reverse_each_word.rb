5fe2e214a9c7a62f3a24cf1c7d43e05e08b3b523
Guligena Aierken <guligena_aierken@163.com>
2019-10-22 21:27:22 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,19 +2,10 @@ require 'pry'
 def reverse_each_word(string)
   array = string.split
   reversed = []
-  array.map do |str|
-  reversed << str.reverse
-  end
-  reversed.join(" ")
-  # binding.pry
-end
-
-ef reverse_each_word(string)
-  array = string.split
-  reversed = []
   array.collect do |str|
   reversed << str.reverse
   end
   reversed.join(" ")
   # binding.pry
 end
+
